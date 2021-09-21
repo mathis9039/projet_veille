f6347eaf45148ba08d452b7f142ff75fd9e41aa9
mathis9039 <mathis9039@gmail.com>
2021-09-21 18:27:13 -0400

    Trafic système

---
+++ speedchase2.0/Assets/Script/NPCNavController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCNavController : MonoBehaviour
+{
+    public float movementSpeed;
+    public float rotationSpeed = 120f;
+    public float stopDistance = 0.2f;
+    Vector3 lastPosition;
+    Vector3 velocity;
+    public Vector3 destination;
+    public bool reachedDestination = false;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != destination)
+        {
+            Vector3 destinationDirection = destination - transform.position;
+            destinationDirection.y = 0;
+
+            float destinationDistance = destinationDirection.magnitude;
+
+            movementSpeed = Random.Range(1f, 5f);
+
+            if (destinationDistance >= stopDistance)
+            {
+                reachedDestination = false;
+                Quaternion targetRotation = Quaternion.LookRotation(destinationDirection);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+                transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+
+            }
+            else
+            {
+                reachedDestination = true;
+            }
+            velocity = (transform.position - lastPosition) / Time.deltaTime;
+            velocity.y = 0;
+            var velocityMagnitude = velocity.magnitude;
+            velocity = velocity.normalized;
+            var fwdDotProduct = Vector3.Dot(transform.forward, velocity);
+            var rightDotProduct = Vector3.Dot(transform.right, velocity);
+        }
+        else
+        {
+            reachedDestination = true;
+        }
+    }
+
+    public void SetDestination(Vector3 destination)
+    {
+        this.destination = destination;
+        reachedDestination = false;
+    }
+}
---
+++ speedchase2.0/Assets/Script/WaypointNavigator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointNavigator : MonoBehaviour
+{
+    NPCNavController controller;
+    public Waypoint currentWaypoint;
+
+    private void Awake(){
+        controller = GetComponent<NPCNavController>();
+    }
+    void Start()
+    {
+        controller.SetDestination(currentWaypoint.GetPosition());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(controller.reachedDestination){
+            currentWaypoint = currentWaypoint.nextWaypoint;
+            controller.SetDestination(currentWaypoint.GetPosition());
+        }
+    }
+}
