02ba0e699b1828535bafb857f38ec7e68d954251
mathis9039 <mathis9039@gmail.com>
2021-10-01 22:53:19 -0400

    finissions
    amélioration du menu
    ajout de NPC
    ajout de policier

--- speedchase2.0/Assets/Script/CarControler.cs
+++ speedchase2.0/Assets/Script/CarControler.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CarControler : MonoBehaviour
 {
@@ -13,6 +14,8 @@ public class CarControler : MonoBehaviour
     private float currentSteerAngle;
     private float currentbreakForce;
     private bool isBreaking;
+    private bool _isCrashed;
+
 
     [SerializeField] private float motorForce;
     [SerializeField] private float breakForce;
@@ -78,9 +81,26 @@ public class CarControler : MonoBehaviour
     private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
     {
         Vector3 pos;
-        Quaternion rot
-;       wheelCollider.GetWorldPose(out pos, out rot);
+        Quaternion rot;    
+        wheelCollider.GetWorldPose(out pos, out rot);
         wheelTransform.rotation = rot;
         wheelTransform.position = pos;
     }
+
+        private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PNG") || other.gameObject.CompareTag("Ocean") || other.gameObject.CompareTag("Obstacle") ||
+            other.gameObject.CompareTag("Trees") || other.gameObject.CompareTag("Batiment") ||  other.gameObject.CompareTag("Fences"))
+        {
+            _isCrashed = true;
+            StartCoroutine(Crash());
+        }
+    }
+        IEnumerator Crash()
+    {
+        yield return new WaitForSeconds(0.15f);
+        SceneManager.LoadScene("Menu");
+    }
+
+
 }
